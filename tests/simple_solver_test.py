8412fa7a48ea70cb655d4229643b67adc0b9895a
yangeorget <yan.georget@polytechnique.org>
2024-04-25 15:46:05 +0200

    add tests

--- tests/simple_solver_test.py
+++ tests/simple_solver_test.py
@@ -6,6 +6,36 @@ from ncs.solvers.simple_solver import SimpleSolver
 
 
 class TestSimpleSolver:
+    def test_solve_and_count(self) -> None:
+        domains = np.array(
+            [
+                [0, 99],
+                [0, 99]
+            ]
+        )
+        problem = Problem(domains)
+        solver = SimpleSolver(problem)
+        counter = 0
+        for _ in solver.solve():
+            counter += 1
+        assert counter == 10000
+
+    def test_solve(self) -> None:
+        domains = np.array(
+            [
+                [0, 1],
+                [0, 1],
+            ]
+        )
+        problem = Problem(domains)
+        solver = SimpleSolver(problem)
+        solutions = [solution for solution in solver.solve()]
+        assert len(solutions) == 4
+        assert np.all(solutions[0] == np.array([[0, 0], [0, 0]]))
+        assert np.all(solutions[1] == np.array([[0, 0], [1, 1]]))
+        assert np.all(solutions[2] == np.array([[1, 1], [0, 0]]))
+        assert np.all(solutions[3] == np.array([[1, 1], [1, 1]]))
+
     def test_solve_sum_1(self) -> None:
         domains = np.array(
             [
