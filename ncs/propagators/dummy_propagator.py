4a5b70a23a8e5a1ffa28e67041bbe59ea774130d
yangeorget <yan.georget@polytechnique.org>
2024-08-05 18:25:37 +0200

    refactor

--- ncs/propagators/dummy_propagator.py
+++ ncs/propagators/dummy_propagator.py
@@ -5,7 +5,7 @@ from numpy.typing import NDArray
 
 
 @jit(nopython=True, cache=True)
-def compute_domains(domains: NDArray) -> Optional[NDArray]:
+def compute_domains(domains: NDArray, data: Optional[NDArray] = None) -> Optional[NDArray]:
     """
     A propagator that does nothing.
     """
