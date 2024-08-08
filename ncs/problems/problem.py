a8df27bf1142e87cd87cd4147258e803e900a7a2
Yan Georget <yan.georget@relevanc.com>
2024-08-08 17:52:02 +0200

    clean up

--- ncs/problems/problem.py
+++ ncs/problems/problem.py
@@ -197,10 +197,10 @@ def filter(
         prop_var_end = var_bounds[prop_idx, END]
         prop_indices = props_indices[prop_var_start:prop_var_end]
         prop_offsets = props_offsets[prop_var_start:prop_var_end].reshape((-1, 1))
-        prop_data_start = data_bounds[prop_idx, START]
-        prop_data_end = data_bounds[prop_idx, END]
         prop_domains = compute_domains(
-            algorithms[prop_idx], shr_domains[prop_indices] + prop_offsets, props_data[prop_data_start:prop_data_end]
+            algorithms[prop_idx],
+            shr_domains[prop_indices] + prop_offsets,
+            props_data[data_bounds[prop_idx, START] : data_bounds[prop_idx, END]],
         )
         if prop_domains is None:
             statistics[STATS_PROPAGATOR_INCONSISTENCY_NB] += 1
