45bd0d8c684b31d4d5e6b80b75e9de5ec7710ef9
Yan Georget <yan.georget@relevanc.com>
2024-12-02 10:47:26 +0100

    add file with TSP instances

---
+++ nucs/examples/tsp/__main__.py
@@ -0,0 +1,37 @@
+###############################################################################
+# __   _            _____    _____
+# | \ | |          / ____|  / ____|
+# |  \| |  _   _  | |      | (___
+# | . ` | | | | | | |       \___ \
+# | |\  | | |_| | | |____   ____) |
+# |_| \_|  \__,_|  \_____| |_____/
+#
+# Fast constraint solving in Python  - https://github.com/yangeorget/nucs
+#
+# Copyright 2024 - Yan Georget
+###############################################################################
+import argparse
+
+from rich import print
+
+from nucs.constants import LOG_LEVEL_INFO, LOG_LEVELS
+from nucs.examples.alpha.alpha_problem import AlphaProblem
+from nucs.heuristics.heuristics import DOM_HEURISTIC_MIN_VALUE, VAR_HEURISTIC_SMALLEST_DOMAIN
+from nucs.solvers.backtrack_solver import BacktrackSolver
+
+# Run with the following command (the second run is much faster because the code has been compiled):
+# NUMBA_CACHE_DIR=.numba/cache python -m nucs.examples.alpha
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--log_level", choices=LOG_LEVELS, default=LOG_LEVEL_INFO)
+    args = parser.parse_args()
+    problem = AlphaProblem()
+    solver = BacktrackSolver(
+        problem,
+        var_heuristic_idx=VAR_HEURISTIC_SMALLEST_DOMAIN,
+        dom_heuristic_idx=DOM_HEURISTIC_MIN_VALUE,
+        log_level=args.log_level,
+    )
+    solutions = solver.find_all()
+    print(solver.get_statistics())
+    print(problem.solution_as_dict(solutions[0]))
