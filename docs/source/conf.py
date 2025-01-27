813528b81ff662ab753b13224bbebb26271501ab
Yan Georget <yan.georget@relevanc.com>
2025-01-27 17:53:33 +0100

    fix version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ project = 'NuCS'
 copyright = '2024-2025, Yan Georget'
 author = 'Yan Georget'
 
-release = '4.7.0'
-version = '4.7.0'
+release = '4.8.0'
+version = '4.8.0'
 
 # -- General configuration
 
